ce08db7df40f5bb78ebafcd8e150dff9462d0e6e
Peter Wells <peterwells723@yahoo.com>
2013-07-25 12:57:39 -0400

    in section 5.12

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,11 +1,19 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def show
