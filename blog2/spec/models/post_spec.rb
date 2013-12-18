51acfc49845b7ffb72739ec3ac115dacd5d1bf76
Peter Wells <peterwells723@yahoo.com>
2013-12-18 13:45:42 -0500

    into 5.11, next get to check that the posts controller recovers from trying to save an invalid post object

--- blog2/spec/models/post_spec.rb
+++ blog2/spec/models/post_spec.rb
@@ -6,7 +6,23 @@ end
 
 describe Post do
   it "can create new Post" do
-    Post.create
+    Post.create(:title => 'A post', :text => 'lalala')
     expect(Post.count).to eq 1
+  end
+  
+  context "validates data" do
+    it "passes with title:'First Post' and text:'Hello World'" do
+      Post.create(:title => 'First Post', :text => 'Hello World')
+      expect(Post.count).to eq 1
+    end
+    
+    it "must have a title" do
+      expect(Post.create(:text => "Hello World")).to have(2).errors_on(:title)
+    end
+    
+    it "fails when title less than five characters" do
+      expect(Post.create(:title => 'four', :text => 'Hello World')
+      ).to have(1).errors_on(:title)
+    end
   end
 end
